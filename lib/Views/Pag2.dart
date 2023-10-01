85e9e1c5c71deb41bf8274a97679fa2944eb78f6
ElanTic <aplicacionesdejon@gmail.com>
2023-10-01 15:07:54 -0600

    agregada barra navegacion

---
+++ lib/Views/Pag2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Pag2 extends StatefulWidget {
+  const Pag2({super.key});
+
+  @override
+  State<Pag2> createState() => _Pag2State();
+}
+
+class _Pag2State extends State<Pag2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("otro"),
+      ),
+    );
+  }
+}
