9acf8e9d03f43eaad97c962cac38ed2e79ad4dc8
Hector Gonzalez <20201010@uaz.edu.mx>
2023-10-05 21:54:41 -0600

    Se agrego la vista info, se modifico el modelo pertenencia y se modifico pag

--- lib/Views/Pag.dart
+++ lib/Views/Pag.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:itemtrackers/models/Joven.dart';
 import 'package:itemtrackers/rest/RDS_joven.dart';
+import 'package:itemtrackers/models/Pertenencia.dart';
 
 class Pag extends StatefulWidget {
   const Pag({super.key});
@@ -13,6 +14,7 @@ class Pag extends StatefulWidget {
 
 class _PagState extends State<Pag> {
   late Future<Joven> futureJoven;
+  List<Pertenencia> lista = [];
 
   @override
   void initState() {
@@ -24,26 +26,48 @@ class _PagState extends State<Pag> {
 
   @override
   Widget build(BuildContext context) {
+    lista = [
+      Pertenencia(
+          id: 0,
+          dueno: 0,
+          nombre: 'algo',
+          color: 'si',
+          marca: 'si',
+          modelo: 'si',
+          descripcion: 'una cosa qeu se me perdio jaja',
+          foto:
+              'https://static.wikia.nocookie.net/sanicman/images/d/d3/Ceyr7zdbxw821.png/revision/latest?cb=20200618024705'),
+      Pertenencia(
+          id: 0,
+          dueno: 0,
+          nombre: 'otra cosa',
+          color: 'no',
+          marca: 'ola',
+          modelo: 'adiso',
+          descripcion: 'otra casa que se ne perdio por ai jijija',
+          foto:
+              'https://pm1.aminoapps.com/7088/14d09e999300a7440c0ce49b4d62873e22f10477r1-546-439v2_hq.jpg'),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Inicio"),
+        title: const Text("Publicaciones"),
       ),
-      body: Center(
-        child: FutureBuilder<Joven>(
-          future: futureJoven,
-          builder: (context, snapshot) {
-            String? tel = "";
-            if (snapshot.hasData) {
-              if (snapshot.data?.telefono != null) {
-                tel = snapshot.data?.telefono;
-              }
-              return Text(snapshot.data!.nombre + " mi telefono es " + tel!);
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: lista.length,
+          itemBuilder: (context, index) {
+            final item = lista[index];
+            return Pertenencia(
+                id: item.id,
+                dueno: item.dueno,
+                nombre: item.nombre,
+                color: item.color,
+                marca: item.marca,
+                modelo: item.modelo,
+                descripcion: item.descripcion,
+                foto: item.foto);
           },
         ),
       ),
--- lib/models/Pertenencia.dart
+++ lib/models/Pertenencia.dart
@@ -1,26 +1,28 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:itemtrackers/Views/Info.dart';
 
-class Pertenencia {
+class Pertenencia extends StatefulWidget {
   int id;
   int dueno;
   String nombre;
-  String? categoria;
-  String? marca;
-  String? modelo;
-  String? color; // Assuming color is represented as a String
-  String? descripcion;
-  String? foto;
+  String categoria;
+  String marca;
+  String modelo;
+  String color; // Assuming color is represented as a String
+  String descripcion;
+  String foto;
 
   Pertenencia({
     required this.id,
     required this.dueno,
     required this.nombre,
-    this.categoria,
-    this.marca,
-    this.modelo,
-    this.color,
-    this.descripcion,
-    this.foto,
+    this.categoria = '',
+    this.marca = '',
+    this.modelo = '',
+    this.color = '',
+    this.descripcion = '',
+    this.foto = '',
   });
 
   factory Pertenencia.fromJson(Map<String, dynamic> json) {
@@ -49,5 +51,104 @@ class Pertenencia {
       'descripcion': descripcion,
       'foto': foto,
     };
+  }
+
+  @override
+  State<StatefulWidget> createState() => _Pertenencia();
+}
+
+class _Pertenencia extends State<Pertenencia> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: ((context) => Info(
+                      color: widget.color,
+                      marca: widget.marca,
+                      modelo: widget.modelo,
+                      nombre: widget.nombre,
+                      foto: widget.foto,
+                      descripcion: widget.descripcion))));
+        },
+        splashColor: Colors.blueGrey,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: Colors.grey, width: 1)),
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(16)),
+                        image: DecorationImage(
+                          image: NetworkImage(widget.foto),
+                        )),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(color: Colors.white54),
+                    child: Text(
+                      widget.nombre,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  _createPersonInfoRow(
+                      key: widget.color, value: 'color', context: context),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  _createPersonInfoRow(
+                      key: widget.marca, value: 'marca', context: context),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  _createPersonInfoRow(
+                      key: widget.modelo, value: 'modelo', context: context),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _createPersonInfoRow(
+      {required String key,
+      required String value,
+      required BuildContext context}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Text(
+            '$value:',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+        const Spacer(),
+        Expanded(
+          child: Text(
+            key,
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        )
+      ],
+    );
   }
 }
